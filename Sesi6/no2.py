393c9be7ce926316b2d1abdb934c3c626e44ba85
analilll <114587597+CecepNurjamannn@users.noreply.github.com>
2024-03-28 23:12:35 +0700

    Add files via upload

---
+++ Sesi6/no2.py
@@ -0,0 +1,7 @@
+for i in range (1, 6):
+    num = i
+    for j in range (0, i):
+        print(num, end=" ")
+        num += 1
+    print()
+
---
+++ Sesi6/no5.py
@@ -0,0 +1,7 @@
+for i in range (1, 6):
+    num = i
+    for j in range (1, 6):
+        print(num, end=" ")
+        num += 2 + i - 1
+        
+    print()
